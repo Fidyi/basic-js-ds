c03d83f1b7edf2c0acfe54ca94610b0c4b1c02f9
serega <tonatos94@gmail.com>
2023-04-17 21:52:24 +0300

    feat:add queue

--- src/queue.js
+++ src/queue.js
@@ -15,19 +15,32 @@ const { ListNode } = require('../extensions/list-node.js');
  */
 class Queue {
   constructor() {
-    this.items = []
+    this.head = null;
+    this.tail = null;
   }
-
   getUnderlyingList() {
-  
+    return this.head;
   }
-
   enqueue(value) {
-
+    const node = new ListNode(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   }
-
   dequeue() {
-
+  if (!this.head) {
+  return null;
+  }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return value;
 }
 }
 
